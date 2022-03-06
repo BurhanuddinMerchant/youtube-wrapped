07fb6d225ffff3d9df085e7bc4e86db13a21b4ce
Burhanuddin Merchant <bmerchant945@gmail.com>
2022-03-06 17:35:44 +0530

    updated about, added home button to dashboard

--- components/Stats.js
+++ components/Stats.js
@@ -6,8 +6,8 @@ export default function StatsSection(props) {
   const [processedData, setProcessedData] = useState(null)
   useEffect(() => {
     setProcessedData({ ...getProcessedStats(stats) })
-    console.log(processedData)
-    console.log(stats)
+    // console.log(processedData)
+    // console.log(stats)
   }, [])
   return (
     <>
