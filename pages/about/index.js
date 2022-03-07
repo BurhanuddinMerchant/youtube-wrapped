5995d1864cc24f6bf297a7c1af9af4f1706ed9db
Burhanuddin Merchant <bmerchant945@gmail.com>
2022-03-07 10:38:38 +0530

    updated contact page ui

--- pages/about/index.js
+++ pages/about/index.js
@@ -25,7 +25,7 @@ export default function About() {
         <Zoom>
           <div className="mx-auto mt-10 w-full rounded-lg bg-white p-10 shadow-md sm:w-2/3">
             <Fade left>
-              <div className="mx-auto w-fit text-2xl font-medium text-red-700">
+              <div className="mx-auto w-fit text-center text-3xl font-medium text-red-700 sm:text-4xl">
                 About Youtube Wrapped
               </div>
             </Fade>
@@ -44,13 +44,13 @@ export default function About() {
                   statistics and data. You just need to Sign-Up to Youtube
                   Wrapped, and authorize the application, thats it! Your Wrap is
                   ready to be served to you. There are a large number of metrics
-                  you get access to ,that will give you insights about your
+                  you get access to, that will give you insights about your
                   Youtube watching habbits.
                 </p>
                 <p className="mt-2 text-left">
                   This project is inspired by an analogous thing done by
                   Spotify, called Spotify Wrapped . Towards the end of last
-                  year, in December 2021 , I got my very first
+                  year, in December 2021, I got my very first
                   <span className="mx-1 font-medium text-green-600">
                     <a
                       href="https://spotify.burhanuddinmerchant.ml/"
@@ -61,14 +61,14 @@ export default function About() {
                     </a>
                   </span>
                   , and since then, I knew I had to do something similar to that
-                  , related to Youtube. So here we go , this is YouTube Wrapped
+                  , related to Youtube. So here we go, this is YouTube Wrapped
                   for you.
                 </p>
               </div>
             </Fade>
             <hr className="mt-5" />
             <Fade left>
-              <div className="mx-auto mt-5 w-fit text-2xl font-medium text-red-700 ">
+              <div className="mx-auto mt-5 w-fit text-center text-3xl font-medium text-red-700 sm:text-4xl ">
                 About Me
               </div>
             </Fade>
@@ -108,7 +108,7 @@ export default function About() {
                   world .... blah blah blah.
                 </p>
                 <p className="mt-2 text-left">
-                  For those of you interested in the blah blah blahs , please
+                  For those of you interested in the blah blah blahs, please
                   visit
                   <a
                     href="https://burhanuddinmerchant.ml"
@@ -123,7 +123,7 @@ export default function About() {
             </Fade>
             <hr className="mt-5" />
             <Fade right>
-              <div className="mx-auto mt-5 w-fit text-2xl font-medium text-red-700">
+              <div className="mx-auto mt-5 w-fit text-center text-3xl font-medium text-red-700 sm:text-4xl">
                 About The Project
               </div>
             </Fade>
@@ -136,16 +136,24 @@ export default function About() {
 
               <div className="mt-10 text-center text-lg">
                 <p className="text-left">
-                  This is a Full Stack Web Application built around an ETL data
-                  pipeline.
+                  This is a Full Stack Web Application built around an{' '}
+                  <a
+                    href="https://en.wikipedia.org/wiki/Extract,_transform,_load"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="text-yellow-700"
+                  >
+                    ETL{' '}
+                  </a>
+                  data pipeline.
                 </p>
                 <p className="mt-2 text-left">
-                  I wanted to set up a Data-Pipeline from a very long time, and
-                  also improve upon my Front-End development skills (UI and
+                  I wanted to set up an ETL Data-Pipeline from a very long time,
+                  and also improve upon my Front-End development skills (UI and
                   Design), and at the same time wanted to do something related
                   to Cloud and DevOps, so when I randomly came up with a
                   meaningful and fun (according to me XD) problem statement, all
-                  of the desires listed above just fell into place , and I
+                  of the desires listed above just fell into place, and I
                   started to work on what you are seeing right now.
                 </p>
                 <hr className="mt-5" />
--- pages/contact/index.js
+++ pages/contact/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import { Fade, Zoom } from 'react-reveal'
 import { useState } from 'react'
 import NavBar from '../../components/Navbar'
-
+import contactSVG from '../../assets/images/contact.svg'
 export default function Contact() {
   const [formState, setFormState] = useState({
     email: '',
@@ -64,51 +64,59 @@ export default function Contact() {
         <Zoom>
           <div className="mx-auto mt-10 w-full rounded-lg bg-white p-10 shadow-md sm:w-2/3">
             <Fade left>
-              <div className="mx-auto w-fit text-2xl font-medium text-red-700">
+              <div className="mx-auto w-fit  text-3xl font-medium text-red-700 sm:text-4xl">
                 Contact
               </div>
-            </Fade>
-            <Fade left>
-              <div className="mt-10 text-center text-lg">
-                <form
-                  className="mx-auto flex w-fit flex-col text-xl"
-                  onSubmit={handleSubmit}
-                >
-                  <input
-                    type="email"
-                    className="rounded-lg border-2 border-red-500 bg-red-200 p-2 placeholder-slate-500 sm:border-white sm:hover:border-red-500 "
-                    placeholder="Enter your email"
-                    required
-                    value={formState.email}
-                    onChange={handleChange}
-                    name="email"
-                  />
-                  <input
-                    type="text"
-                    className="mt-2 rounded-lg border-2 border-red-500 bg-red-200 p-2 placeholder-slate-500  sm:border-white sm:hover:border-red-500"
-                    placeholder="Enter Your Full Name"
-                    required
-                    value={formState.name}
-                    onChange={handleChange}
-                    name="name"
-                  />
-                  <textarea
-                    placeholder="Message"
-                    className=" mt-2 h-32 rounded-lg border-2 border-red-500 bg-red-200 p-2 placeholder-slate-500  sm:border-white sm:hover:border-red-500"
-                    value={formState.message}
-                    onChange={handleChange}
-                    name="message"
-                    required
-                  />
-                  <button
-                    type="submit"
-                    className=" mx-auto mt-2 w-fit cursor-pointer rounded-lg border-2  border-red-500 bg-red-500 p-1 text-lg text-white hover:bg-white hover:text-black"
-                  >
-                    Submit
-                  </button>
-                </form>
+              <div className="mx-auto mt-2 w-fit text-lg font-medium text-gray-500">
+                I Would Love To Hear From You !
               </div>
             </Fade>
+            <div className="mx-auto my-auto flex h-fit w-fit flex-col-reverse rounded-lg  bg-none sm:flex-row sm:bg-white xl:p-10">
+              <div className="my-auto mx-0 hidden w-full sm:mr-10  sm:w-96 xl:block">
+                <img src={contactSVG.src} className="w-full" />
+              </div>
+              <Fade left>
+                <div className="mx-auto mt-10 text-center text-lg">
+                  <form
+                    className="mx-auto flex w-fit flex-col text-xl"
+                    onSubmit={handleSubmit}
+                  >
+                    <input
+                      type="email"
+                      className="rounded-lg border-2 border-red-500 bg-red-200 p-2 placeholder-slate-500 sm:border-white sm:hover:border-red-500 "
+                      placeholder="Enter your email"
+                      required
+                      value={formState.email}
+                      onChange={handleChange}
+                      name="email"
+                    />
+                    <input
+                      type="text"
+                      className="mt-2 rounded-lg border-2 border-red-500 bg-red-200 p-2 placeholder-slate-500  sm:border-white sm:hover:border-red-500"
+                      placeholder="Enter Your Full Name"
+                      required
+                      value={formState.name}
+                      onChange={handleChange}
+                      name="name"
+                    />
+                    <textarea
+                      placeholder="Message"
+                      className=" mt-2 h-32 rounded-lg border-2 border-red-500 bg-red-200 p-2 placeholder-slate-500  sm:border-white sm:hover:border-red-500"
+                      value={formState.message}
+                      onChange={handleChange}
+                      name="message"
+                      required
+                    />
+                    <button
+                      type="submit"
+                      className=" mx-auto mt-2 w-fit cursor-pointer rounded-lg border-2  border-red-500 bg-red-500 p-1 text-lg text-white hover:bg-white hover:text-black"
+                    >
+                      Submit
+                    </button>
+                  </form>
+                </div>
+              </Fade>
+            </div>
           </div>
         </Zoom>
       </div>
