e3e087bc80696dc052de45b84524d8b29ed5b0e2
Burhanuddin Merchant <bmerchant945@gmail.com>
2022-03-24 23:14:22 +0530

    used window property instead of global object

--- pages/recaptcha/index.js
+++ pages/recaptcha/index.js
@@ -6,16 +6,18 @@ export default function Recaptcha() {
     const res = await loadScript(
       `https://www.google.com/recaptcha/api.js?render=${process.env['NEXT_PUBLIC_RECAPTCHA_SITE_KEY']}`
     )
+    if (!res) {
+    }
   }, [])
   function handleClick() {
-    grecaptcha.ready(async function () {
-      grecaptcha
+    window.grecaptcha.ready(async function () {
+      window.grecaptcha
         .execute(process.env['NEXT_PUBLIC_RECAPTCHA_SITE_KEY'], {
           action: 'submit',
         })
         .then(async (token) => {
           let response = await fetch(
-            `${process.env['NEXT_PUBLIC_SERVER_BASE_URL']}/api/recaptcha?token=${token}hel`
+            `${process.env['NEXT_PUBLIC_SERVER_BASE_URL']}/api/recaptcha?token=${token}`
           )
           response = await response.json()
           setIsHuman(response['verified'])
