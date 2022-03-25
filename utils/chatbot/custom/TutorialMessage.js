3feaf2ee7168005c3a103ec9be8b4cc1d9b8685c
Burhanuddin Merchant <bmerchant945@gmail.com>
2022-03-25 23:07:41 +0530

    added walkthrought to cb as well

--- utils/chatbot/custom/TutorialMessage.js
+++ utils/chatbot/custom/TutorialMessage.js
@@ -2,7 +2,7 @@ export const TutorialMessage = (props) => {
   return (
     <div className="w-fit cursor-pointer rounded-full border-2 border-red-400 p-1 hover:bg-red-400 hover:text-white">
       <a
-        href="https://www.youtube.com/watch?v=i0stGfnvdlg"
+        href="https://www.youtube.com/watch?v=V7JnDTsLDbk"
         target="_blank"
         rel="noreferrer"
       >
