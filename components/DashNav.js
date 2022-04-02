084a5e5e3840e846d523c12496731cbfc9f0e56a
Burhanuddin Merchant <bmerchant945@gmail.com>
2022-04-02 18:02:11 +0530

    minor ui tweaks in the navbar for profile

--- components/DashNav.js
+++ components/DashNav.js
@@ -10,26 +10,26 @@ export default function DashNav(props) {
   }
   const router = useRouter()
   return (
-    <div className="flex w-full flex-col  justify-between bg-gray-700 p-3 sm:flex-row">
+    <div className="my-auto flex w-full flex-row justify-between bg-gray-700 p-3">
       <div
-        className=" mx-auto flex w-fit rounded-full bg-gray-900 px-2 hover:cursor-pointer hover:bg-gray-800 sm:mx-0"
+        className=" flex w-fit cursor-pointer rounded-full bg-gray-900 px-2 hover:bg-gray-800 "
         onClick={() => router.push('/profile')}
       >
         <div className="h-10 w-10 rounded-full bg-white p-2">
           <img src={`https://avatars.dicebear.com/api/human/${username}.svg`} />
         </div>
-        <h2 className="my-auto ml-1 cursor-default text-xl font-semibold text-red-400">
+        <h2 className="my-auto ml-1 text-xl font-semibold text-red-400">
           {username}
         </h2>
       </div>
 
-      <div className="mx-auto flex w-fit sm:mx-0">
+      <div className="my-auto flex w-fit">
         <HomeIcon
-          className="my-auto mt-2 mr-2 h-8 w-8 cursor-pointer text-red-400 hover:text-white sm:my-auto"
+          className="my-auto mr-2 h-8 w-8 cursor-pointer text-red-400 hover:text-white sm:my-auto"
           onClick={() => router.push('/')}
         />
         <button
-          className="mx-auto  mt-2 w-fit cursor-pointer rounded-md bg-red-400  px-2 py-1 text-red-100 hover:bg-white hover:text-black sm:mx-0 sm:my-auto"
+          className="mx-auto  w-fit cursor-pointer rounded-md bg-red-400  px-2 py-1 text-red-100 hover:bg-white hover:text-black sm:mx-0 sm:my-auto"
           onClick={handleSignOut}
         >
           <Link href="/login">SignOut</Link>
