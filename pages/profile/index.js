569bd4cc33f125f44dc2cbe68cb6f672df077cd5
Burhanuddin Merchant <bmerchant945@gmail.com>
2022-04-02 11:37:48 +0530

    updated ui for profile section

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,9 +1,12 @@
-import { ArrowCircleLeftIcon } from '@heroicons/react/solid'
+import {
+  ArrowCircleLeftIcon,
+  CheckCircleIcon,
+  XCircleIcon,
+} from '@heroicons/react/solid'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import axiosApiInstance from '../../utils/axiosConfig'
-
 export default function Profile() {
   const router = useRouter()
   const [data, setData] = useState({
@@ -46,73 +49,74 @@ export default function Profile() {
         />
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <div className="flex w-full flex-col  justify-between bg-gray-700 p-3 sm:flex-row">
-        <div
-          className="my-auto"
-          onClick={() => {
-            router.push('/dashboard')
-          }}
-        >
-          <ArrowCircleLeftIcon className="rounded-ful h-10 w-10 cursor-pointer text-red-500 hover:text-red-400" />
-        </div>
-        <div className="flex">
-          <div className="h-10 w-10 rounded-full bg-red-50 p-2">
-            <img src="https://avatars.dicebear.com/api/human/love.svg" />
-          </div>
-          <h2 className="my-auto ml-1 cursor-default text-xl font-semibold text-red-400">
-            Profile
-          </h2>
-        </div>
-      </div>
       <div className="h-screen bg-red-100">
-        <div className="mx-auto w-full bg-red-100 pt-10  md:w-1/2">
-          <div className="mx-auto w-full rounded-lg bg-white p-5 shadow-lg">
-            <div className="mx-auto h-28 w-28 rounded-full bg-white">
-              <img src="https://avatars.dicebear.com/api/human/love.svg" />
-            </div>
-            <div className="mx-auto flex w-1/3 justify-between">
-              <div className="my-auto">Name:</div>
-              <input
-                value={data.username}
-                disabled
-                className="mt-2 bg-red-200 p-1"
+        <div className="flex w-full flex-row justify-between bg-gray-700 p-3">
+          <div
+            className="my-auto"
+            onClick={() => {
+              router.push('/dashboard')
+            }}
+          >
+            <ArrowCircleLeftIcon className="rounded-ful h-10 w-10 cursor-pointer text-red-500 hover:text-red-400" />
+          </div>
+          <div className="flex">
+            <div className="h-10 w-10 rounded-full bg-red-50 p-2">
+              <img
+                src={`https://avatars.dicebear.com/api/human/${data.username}.svg`}
               />
             </div>
-            <div className="mx-auto mt-2 flex w-1/3 justify-between">
-              <div className="my-auto">Email : </div>
-              <input
-                value={data.email}
-                disabled
-                className="mt-2 bg-red-200 p-1"
-              />
-            </div>
-            <div className="mx-auto mt-2 flex w-1/3 justify-between">
-              <div className="my-auto">Active : </div>
-              <input
-                disabled
-                type="checkbox"
-                checked={data.active}
-                className="my-auto bg-red-200 p-1"
-              />
-            </div>
-            <div className="mx-auto mt-2 flex w-1/3 justify-between">
-              <div className="my-auto">Stats : </div>
-              <input
-                disabled
-                type="checkbox"
-                checked={data.stats}
-                className="my-auto p-1"
-              />
-            </div>
-            <div className="mx-auto mt-2 flex w-1/3 justify-between">
-              <div className="my-auto">Joined On : </div>
-              <input
-                disabled
-                type="date"
-                value={data.joined.split('T')[0]}
-                className="my-auto bg-red-200 p-1"
-              />
-            </div>
+            <h2 className="my-auto ml-1 cursor-default text-xl font-semibold text-red-400">
+              Profile
+            </h2>
+          </div>
+        </div>
+
+        <div className=" my-24 mx-auto w-full rounded-lg bg-white p-5 shadow-lg md:w-11/12 xl:w-1/2">
+          <div className="mx-auto h-28 w-28 rounded-full bg-red-100">
+            <img
+              src={`https://avatars.dicebear.com/api/human/${data.username}.svg`}
+            />
+          </div>
+          <div className="mx-auto mt-2 flex w-full justify-between rounded-xl border-0 border-red-500 bg-red-50 p-2 shadow-sm md:w-2/3">
+            <div className="my-auto w-1/2 ">Name:</div>
+            <input
+              value={data.username}
+              disabled
+              className="w-1/2 rounded-xl bg-red-200 p-2"
+            />
+          </div>
+          <div className="mx-auto mt-2 flex w-full justify-between rounded-xl border-0 border-red-500 bg-red-50 p-2 shadow-sm md:w-2/3">
+            <div className="my-auto w-1/2">Email : </div>
+            <input
+              value={data.email}
+              disabled
+              className="w-1/2 rounded-xl bg-red-200 p-2"
+            />
+          </div>
+          <div className="mx-auto mt-2 flex w-full justify-between rounded-xl border-0 border-red-500 bg-red-50 p-2 shadow-sm md:w-2/3">
+            <div className="my-auto w-1/2">Active : </div>
+            {data.active ? (
+              <CheckCircleIcon className="mx-auto  my-auto h-7 w-7 text-green-600" />
+            ) : (
+              <XCircleIcon className="mx-auto my-auto h-7 w-7 text-red-600" />
+            )}
+          </div>
+          <div className="mx-auto mt-2 flex w-full justify-between rounded-xl border-0 border-red-500 bg-red-50 p-2 shadow-sm md:w-2/3">
+            <div className="my-auto w-1/2">Stats : </div>
+            {data.stats ? (
+              <CheckCircleIcon className="mx-auto  my-auto h-7 w-7 text-green-600" />
+            ) : (
+              <XCircleIcon className="mx-auto my-auto h-7 w-7 text-red-600" />
+            )}
+          </div>
+          <div className="mx-auto mt-2 flex w-full justify-between rounded-xl border-0 border-red-500 bg-red-50 p-2 shadow-sm md:w-2/3">
+            <div className="my-auto w-1/2">Joined On : </div>
+            <input
+              disabled
+              type="date"
+              value={data.joined.split('T')[0]}
+              className="my-auto w-1/2 rounded-xl bg-red-200 p-2"
+            />
           </div>
         </div>
       </div>
